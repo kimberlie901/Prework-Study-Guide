f8d8cc859a43b43ade594e8184a49fa8e4da9132
Kimmy <kimberlieguillaume@gmail.com>
2023-02-25 21:29:48 -0500

    Added functions to JavaScript Study Guide

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,48 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
+function listTopics() {
 
-    console.log(topics[x]);
+    for (var x = 0; x < topics.length; x++) {
+
+        console.log(topics[x]);
+    }
 }
 
 
+function selectTopic() {
 
-/*
+    
+    if (randomTopic === 'HTML') {
 
-if (topic === 'HTML') {
+        console.log("Let's study HTML!");
 
-    console.log("Let's study HTML!");
+}   else if (randomTopic === 'CSS') {
 
-} else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
 
-    console.log("Let's study CSS!");
+}   else if (randomTopic === 'Git') {
 
-} else if (topic === 'Git') {
+        console.log("Let's study Git");
 
-    console.log("Let's study Git");
+}   else if (randomTopic === 'JavaScript') {
 
-} else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript");
 
-    console.log("Let's study JavaScript");
+}   else {
 
-} else {
-
-    console.log('Please try again!');
+        console.log('Please try again!');
 }
 
-*/
+
+}
+
+
+console.log('Here are the topics we learned through Prework:');
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopic()
